fbc9b10d0dd686e554d28e2a298c60546fb0b66f
tauseefiqbal <tauseef.iqbal@live.com>
2026-03-05 05:29:35 -0700

    Delete spring-boot-library directory

--- spring-boot-library/src/main/resources/schema-update.sql
+++
@@ -1,5 +0,0 @@
--- Update img column length in history table to support base64 images
-ALTER TABLE history MODIFY COLUMN img LONGTEXT;
-
--- Update img column length in book table to support base64 images
-ALTER TABLE book MODIFY COLUMN img LONGTEXT;
